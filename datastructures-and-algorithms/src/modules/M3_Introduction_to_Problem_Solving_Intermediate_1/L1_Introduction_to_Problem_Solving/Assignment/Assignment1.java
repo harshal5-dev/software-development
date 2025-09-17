72b9a69027bd9b298fd201ead0f5df994aced85b
harshal5-dev <harshalganbote55@gmail.com>
2025-09-17 20:29:46 +0530

    fix: Update wording in factor description for clarity

--- datastructures-and-algorithms/src/modules/M3_Introduction_to_Problem_Solving_Intermediate_1/L1_Introduction_to_Problem_Solving/Assignment/Assignment1.java
+++ datastructures-and-algorithms/src/modules/M3_Introduction_to_Problem_Solving_Intermediate_1/L1_Introduction_to_Problem_Solving/Assignment/Assignment1.java
@@ -5,7 +5,7 @@ public class Assignment1 {
   /*
    * Problem Description
    * Given an integer A, you need to find the count of its factors.
-   * Factor of a number is the number which divides it perfectly, leaving no
+   * Factor of a number is the number that divides it perfectly, leaving no
    * remainder.
    *
    * Example: 1, 2, 3, 6 are factors of 6
